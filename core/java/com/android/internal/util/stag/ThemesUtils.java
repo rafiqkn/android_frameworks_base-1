DerpLab/platform_frameworks_base

base: Switch Styles [1/3]
@ZeNiXxX :Add stripped version of theme engine (based on DU Themes implementation)
@idoybh edits: Add setting to backup and document

Signed-off-by: Aman Singh <amankumarsingh1412@gmail.com>

core/java/com/android/internal/util/aosip/ThemesUtils.java
@@ -1,0 +1,72 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ * Copyright (C) 2020 ZenX-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.aosip;
+
+import static android.os.UserHandle.USER_SYSTEM;
+
+import android.app.UiModeManager;
+import android.content.Context;
+import android.content.om.IOverlayManager;
+import android.content.om.OverlayInfo;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.provider.Settings;
+import android.os.RemoteException;
+import android.util.Log;
+
+public class ThemesUtils {
+
+    public static final String TAG = "ThemesUtils";
+
+    // Switch themes
+    private static final String[] SWITCH_THEMES = {
+        "com.android.system.switch.stock", // 0
+        "com.android.system.switch.oneplus", // 1
+        "com.android.system.switch.narrow", // 2
+        "com.android.system.switch.contained", // 3
+        "com.android.system.switch.telegram", // 4
+        "com.android.system.switch.md2", // 5
+        "com.android.system.switch.retro", // 6
+    };
+
+    public static void updateSwitchStyle(IOverlayManager om, int userId, int switchStyle) {
+        if (switchStyle == 0) {
+            stockSwitchStyle(om, userId);
+        } else {
+            try {
+                om.setEnabled(SWITCH_THEMES[switchStyle],
+                        true, userId);
+            } catch (RemoteException e) {
+                Log.w(TAG, "Can't change switch theme", e);
+            }
+        }
+    }
+
+    public static void stockSwitchStyle(IOverlayManager om, int userId) {
+        for (int i = 0; i < SWITCH_THEMES.length; i++) {
+            String switchtheme = SWITCH_THEMES[i];
+            try {
+                om.setEnabled(switchtheme,
+                        false /*disable*/, userId);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}

rafiqkn/android_frameworks_base-1

Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

core/java/com/android/internal/util/stag/ThemesUtils.java
@@ -1,0 +1,72 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ * Copyright (C) 2020 ZenX-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.stag;
+
+import static android.os.UserHandle.USER_SYSTEM;
+
+import android.app.UiModeManager;
+import android.content.Context;
+import android.content.om.IOverlayManager;
+import android.content.om.OverlayInfo;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.provider.Settings;
+import android.os.RemoteException;
+import android.util.Log;
+
+public class ThemesUtils {
+
+    public static final String TAG = "ThemesUtils";
+
+    // Switch themes
+    private static final String[] SWITCH_THEMES = {
+        "com.android.system.switch.stock", // 0
+        "com.android.system.switch.oneplus", // 1
+        "com.android.system.switch.narrow", // 2
+        "com.android.system.switch.contained", // 3
+        "com.android.system.switch.telegram", // 4
+        "com.android.system.switch.md2", // 5
+        "com.android.system.switch.retro", // 6
+    };
+
+    public static void updateSwitchStyle(IOverlayManager om, int userId, int switchStyle) {
+        if (switchStyle == 0) {
+            stockSwitchStyle(om, userId);
+        } else {
+            try {
+                om.setEnabled(SWITCH_THEMES[switchStyle],
+                        true, userId);
+            } catch (RemoteException e) {
+                Log.w(TAG, "Can't change switch theme", e);
+            }
+        }
+    }
+
+    public static void stockSwitchStyle(IOverlayManager om, int userId) {
+        for (int i = 0; i < SWITCH_THEMES.length; i++) {
+            String switchtheme = SWITCH_THEMES[i];
+            try {
+                om.setEnabled(switchtheme,
+                        false /*disable*/, userId);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}